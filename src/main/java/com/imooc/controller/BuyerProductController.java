8478324f013273d1ab7a1fba9e4937f67fbd88ae
JeffPan <www.jeffpan@gmail.com>
2017-09-18 08:24:21 +0800

    拼接json

--- src/main/java/com/imooc/controller/BuyerProductController.java
+++ src/main/java/com/imooc/controller/BuyerProductController.java
@@ -7,6 +7,7 @@ import com.imooc.dataobject.ProductCategory;
 import com.imooc.dataobject.ProductInfo;
 import com.imooc.service.CategoryService;
 import com.imooc.service.ProductService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -37,9 +39,27 @@ public class BuyerProductController {
         //查询所有上架商品
         List<ProductInfo> productInfoList = productService.findUpAll();
         //查询类目（一次性查询
-        List<Integer> categoryTypeList = new ArrayList<Integer>();
-        List<ProductCategory>  categoryList = categoryService.findByCategoryTypeIn(categoryTypeList);
+        List<Integer> categoryTypeList = productInfoList.stream().
+                map(e -> e.getCategoryType()).collect(Collectors.toList());
+
+        List<ProductCategory>  productCategoryList  = categoryService.findByCategoryTypeIn(categoryTypeList);
+
         //数据拼装
+        for (ProductCategory productCategory : productCategoryList) {
+            ProductVO productVO = new ProductVO();
+            productVO.setCategoryType(productCategory.getCategoryType());
+            productVO.setCategoryName(productCategory.getCategoryName());
+            List<ProductVO> productVOList = new ArrayList<ProductVO>();
+            List<ProductInfoVO> productInfoVOList = new ArrayList<ProductInfoVO>();
+            for (ProductInfo productInfo : productInfoList) {
+                if (productInfo.getCategoryType().equals(productCategory.getCategoryType())) {
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo, productInfoVO);
+                    productInfoVOList.add(productInfoVO);
+                }
+            }
+            productVO.setProductInfoVOList(productInfoVOList);
+        }
 
         ResultVO resultVO =new ResultVO();
         ProductVO productVO =new ProductVO();
